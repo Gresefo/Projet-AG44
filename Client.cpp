236c848844af7b19be892f99184a508e73483c6d
Gresefo <loic-martin@laposte.net>
2018-12-04 10:54:11 +0100

    qsdf

--- Client.cpp
+++ Client.cpp
@@ -3,9 +3,9 @@
 
 int main()
 {
+	// Creation of graphs using .txt files
 	vector<Graph> G;
 	Graph g1, g2, g3, g4;
-
 	g1.fileToGraph("Files/exampleUndirectedMatrix.txt");
 	G.push_back(g1);
 	g2.fileToGraph("Files/exampleDirectedMatrix.txt");
@@ -15,14 +15,11 @@ int main()
 	g4.fileToGraph("Files/exampleDirectedList.txt");
 	G.push_back(g4);
 
-
 	// Printing out a graph example of each type
 	for (unsigned int i = 0; i < G.size(); i++)
 	{
 		cout << G[i];
 	}
-
-
 
 	return 0;
 }
